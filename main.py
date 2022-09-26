e720227f5c41b6d44d7ac7b93965e89a694c62f6
AXEL COUDROT <coud0011@2A4F1-31UPC166.ad-urca.univ-reims.fr>
2022-09-26 18:01:55 +0200

    modif main.py final

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Copyright 2021 TOM DARET
+# Copyright 2021 TOM DARET et AXEL COUDROT
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
