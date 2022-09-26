c16a33ffdc01ff0ce3926a56f16cacdb5d47d8dc
Tom Daret <daret.tom@gmail.com>
2022-09-26 17:33:45 +0200

    modif convert python

--- functions.py
+++ functions.py
@@ -2,12 +2,31 @@ def convert(n : int,b : int) -> str:
 
     reste=''
     val=0
-    while n >= b:
+
+
+
+    while n+b-1 >= b:
+        
         val = ( n % b)
-        reste+= str(val)
+
+        if val == 10:
+            reste+="A"
+        elif val == 11:
+            reste+="B"
+        elif val == 12:
+            reste+="C"
+        elif val == 13:
+            reste+="D"
+        elif val == 14:
+            reste+="E"
+        elif val == 15:
+            reste+= "F"
+        else:
+            reste+= str(val)
+
         n= n//b
 
-    reste+= str(n)
+
     print (reste[::-1])
 
 
